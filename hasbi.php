f5f5b2e69602ec838d1fc09b2b6656a330e74a7b
hasbinikmalnatsir <162419556+hasbinikmalnatsir@users.noreply.github.com>
2024-03-06 15:20:17 +0700

    Create hasbi.php
    hasbi

---
+++ hasbi.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>kampus</title>
+  </head>
+  <body>
+    <?php
+echo "Hasbi Nikmal Natsir";
+echo "Amik Mahaputra Riau"
+  echo " JLN subrantas"
+  <?
+  </body>
+</html>
